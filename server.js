3073b3c242c9ada68cafa7024610c09439c4c12d
Mahesh <mahesh.marri@gmail.com>
2018-11-17 22:46:38 +1100

    fixed semicolon

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url');
 var server = http.createServer(function(req, res) {
 var page = url.parse(req.url).pathname;
 console.log(page);
-console.log(req.url)
+console.log(req.url);
 res.writeHead(200, {"Content-Type": "text/plain"});
 if (page == '/') {
 res.write('You\'re at the reception desk. How can I help you?');
